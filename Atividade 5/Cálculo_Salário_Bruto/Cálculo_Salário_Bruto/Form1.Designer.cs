501934b0b4624b741dffa757055252a80b9b36e6
Lucas Araújo <80704314+LFeli@users.noreply.github.com>
2021-09-20 12:14:34 -0300

    Add files via upload

---
+++ Atividade 5/Cálculo_Salário_Bruto/Cálculo_Salário_Bruto/Form1.Designer.cs
@@ -0,0 +1,374 @@
+﻿
+namespace Cálculo_Salário_Bruto
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblNomeFunci = new System.Windows.Forms.Label();
+            this.lblSalaBrut = new System.Windows.Forms.Label();
+            this.lblNumFilhos = new System.Windows.Forms.Label();
+            this.lblAliqINSS = new System.Windows.Forms.Label();
+            this.lblAluiqIRPF = new System.Windows.Forms.Label();
+            this.lblSalaFml = new System.Windows.Forms.Label();
+            this.lblDescINSS = new System.Windows.Forms.Label();
+            this.lblDescIPRF = new System.Windows.Forms.Label();
+            this.btnCalcu = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnLimp = new System.Windows.Forms.Button();
+            this.lblSexo = new System.Windows.Forms.Label();
+            this.lblCasado = new System.Windows.Forms.Label();
+            this.cmbCasado = new System.Windows.Forms.ComboBox();
+            this.cmbSexo = new System.Windows.Forms.ComboBox();
+            this.txtDescIPRF = new System.Windows.Forms.TextBox();
+            this.txtDescINSS = new System.Windows.Forms.TextBox();
+            this.txtSalaFml = new System.Windows.Forms.TextBox();
+            this.txtAluiqIPRF = new System.Windows.Forms.TextBox();
+            this.txtAliqINSS = new System.Windows.Forms.TextBox();
+            this.txtNumFilhos = new System.Windows.Forms.TextBox();
+            this.txtSalaBrut = new System.Windows.Forms.TextBox();
+            this.txtNomeFunci = new System.Windows.Forms.TextBox();
+            this.lblSalaLiquido = new System.Windows.Forms.Label();
+            this.txtSalaLiquido = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblNomeFunci
+            // 
+            this.lblNomeFunci.AutoSize = true;
+            this.lblNomeFunci.BackColor = System.Drawing.Color.Transparent;
+            this.lblNomeFunci.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblNomeFunci.Location = new System.Drawing.Point(12, 20);
+            this.lblNomeFunci.Name = "lblNomeFunci";
+            this.lblNomeFunci.Size = new System.Drawing.Size(127, 15);
+            this.lblNomeFunci.TabIndex = 0;
+            this.lblNomeFunci.Text = "Nome do funcionario: ";
+            // 
+            // lblSalaBrut
+            // 
+            this.lblSalaBrut.AutoSize = true;
+            this.lblSalaBrut.BackColor = System.Drawing.Color.Transparent;
+            this.lblSalaBrut.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblSalaBrut.Location = new System.Drawing.Point(12, 48);
+            this.lblSalaBrut.Name = "lblSalaBrut";
+            this.lblSalaBrut.Size = new System.Drawing.Size(80, 15);
+            this.lblSalaBrut.TabIndex = 1;
+            this.lblSalaBrut.Text = "Salário bruto :";
+            // 
+            // lblNumFilhos
+            // 
+            this.lblNumFilhos.AutoSize = true;
+            this.lblNumFilhos.BackColor = System.Drawing.Color.Transparent;
+            this.lblNumFilhos.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblNumFilhos.Location = new System.Drawing.Point(12, 77);
+            this.lblNumFilhos.Name = "lblNumFilhos";
+            this.lblNumFilhos.Size = new System.Drawing.Size(105, 15);
+            this.lblNumFilhos.TabIndex = 2;
+            this.lblNumFilhos.Text = "Número de filhos :";
+            // 
+            // lblAliqINSS
+            // 
+            this.lblAliqINSS.AutoSize = true;
+            this.lblAliqINSS.BackColor = System.Drawing.Color.Transparent;
+            this.lblAliqINSS.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblAliqINSS.Location = new System.Drawing.Point(12, 140);
+            this.lblAliqINSS.Name = "lblAliqINSS";
+            this.lblAliqINSS.Size = new System.Drawing.Size(85, 15);
+            this.lblAliqINSS.TabIndex = 4;
+            this.lblAliqINSS.Text = "Aliquota INSS :";
+            // 
+            // lblAluiqIRPF
+            // 
+            this.lblAluiqIRPF.AutoSize = true;
+            this.lblAluiqIRPF.BackColor = System.Drawing.Color.Transparent;
+            this.lblAluiqIRPF.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblAluiqIRPF.Location = new System.Drawing.Point(12, 174);
+            this.lblAluiqIRPF.Name = "lblAluiqIRPF";
+            this.lblAluiqIRPF.Size = new System.Drawing.Size(84, 15);
+            this.lblAluiqIRPF.TabIndex = 5;
+            this.lblAluiqIRPF.Text = "Aliquota IRPF :";
+            // 
+            // lblSalaFml
+            // 
+            this.lblSalaFml.AutoSize = true;
+            this.lblSalaFml.BackColor = System.Drawing.Color.Transparent;
+            this.lblSalaFml.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblSalaFml.Location = new System.Drawing.Point(11, 204);
+            this.lblSalaFml.Name = "lblSalaFml";
+            this.lblSalaFml.Size = new System.Drawing.Size(89, 15);
+            this.lblSalaFml.TabIndex = 6;
+            this.lblSalaFml.Text = "Salário Família :";
+            // 
+            // lblDescINSS
+            // 
+            this.lblDescINSS.AutoSize = true;
+            this.lblDescINSS.BackColor = System.Drawing.Color.Transparent;
+            this.lblDescINSS.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblDescINSS.Location = new System.Drawing.Point(11, 233);
+            this.lblDescINSS.Name = "lblDescINSS";
+            this.lblDescINSS.Size = new System.Drawing.Size(90, 15);
+            this.lblDescINSS.TabIndex = 7;
+            this.lblDescINSS.Text = "Desconto INSS :";
+            // 
+            // lblDescIPRF
+            // 
+            this.lblDescIPRF.AutoSize = true;
+            this.lblDescIPRF.BackColor = System.Drawing.Color.Transparent;
+            this.lblDescIPRF.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblDescIPRF.Location = new System.Drawing.Point(12, 262);
+            this.lblDescIPRF.Name = "lblDescIPRF";
+            this.lblDescIPRF.Size = new System.Drawing.Size(89, 15);
+            this.lblDescIPRF.TabIndex = 8;
+            this.lblDescIPRF.Text = "Desconto IRPF :";
+            this.lblDescIPRF.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // btnCalcu
+            // 
+            this.btnCalcu.Location = new System.Drawing.Point(121, 358);
+            this.btnCalcu.Name = "btnCalcu";
+            this.btnCalcu.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcu.TabIndex = 9;
+            this.btnCalcu.Text = "Calcular";
+            this.btnCalcu.UseVisualStyleBackColor = true;
+            this.btnCalcu.Click += new System.EventHandler(this.btnCalcu_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(507, 358);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 10;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnLimp
+            // 
+            this.btnLimp.Location = new System.Drawing.Point(321, 358);
+            this.btnLimp.Name = "btnLimp";
+            this.btnLimp.Size = new System.Drawing.Size(75, 23);
+            this.btnLimp.TabIndex = 10;
+            this.btnLimp.Text = "Limpar";
+            this.btnLimp.UseVisualStyleBackColor = true;
+            this.btnLimp.Click += new System.EventHandler(this.btnLimp_Click);
+            // 
+            // lblSexo
+            // 
+            this.lblSexo.AutoSize = true;
+            this.lblSexo.BackColor = System.Drawing.Color.Transparent;
+            this.lblSexo.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblSexo.Location = new System.Drawing.Point(321, 135);
+            this.lblSexo.Name = "lblSexo";
+            this.lblSexo.Size = new System.Drawing.Size(38, 15);
+            this.lblSexo.TabIndex = 11;
+            this.lblSexo.Text = "Sexo :";
+            this.lblSexo.Click += new System.EventHandler(this.lblSexo_Click);
+            // 
+            // lblCasado
+            // 
+            this.lblCasado.AutoSize = true;
+            this.lblCasado.BackColor = System.Drawing.Color.Transparent;
+            this.lblCasado.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblCasado.Location = new System.Drawing.Point(287, 169);
+            this.lblCasado.Name = "lblCasado";
+            this.lblCasado.Size = new System.Drawing.Size(72, 15);
+            this.lblCasado.TabIndex = 12;
+            this.lblCasado.Text = "Casado (a)  :";
+            this.lblCasado.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // cmbCasado
+            // 
+            this.cmbCasado.FormattingEnabled = true;
+            this.cmbCasado.Items.AddRange(new object[] {
+            "Sim ",
+            "Não"});
+            this.cmbCasado.Location = new System.Drawing.Point(365, 166);
+            this.cmbCasado.Name = "cmbCasado";
+            this.cmbCasado.Size = new System.Drawing.Size(121, 23);
+            this.cmbCasado.TabIndex = 13;
+            // 
+            // cmbSexo
+            // 
+            this.cmbSexo.FormattingEnabled = true;
+            this.cmbSexo.Items.AddRange(new object[] {
+            "Masculino ",
+            "Feminino"});
+            this.cmbSexo.Location = new System.Drawing.Point(365, 132);
+            this.cmbSexo.Name = "cmbSexo";
+            this.cmbSexo.Size = new System.Drawing.Size(121, 23);
+            this.cmbSexo.TabIndex = 14;
+            // 
+            // txtDescIPRF
+            // 
+            this.txtDescIPRF.Location = new System.Drawing.Point(148, 259);
+            this.txtDescIPRF.Name = "txtDescIPRF";
+            this.txtDescIPRF.ReadOnly = true;
+            this.txtDescIPRF.Size = new System.Drawing.Size(100, 23);
+            this.txtDescIPRF.TabIndex = 15;
+            // 
+            // txtDescINSS
+            // 
+            this.txtDescINSS.Location = new System.Drawing.Point(148, 230);
+            this.txtDescINSS.Name = "txtDescINSS";
+            this.txtDescINSS.ReadOnly = true;
+            this.txtDescINSS.Size = new System.Drawing.Size(100, 23);
+            this.txtDescINSS.TabIndex = 16;
+            // 
+            // txtSalaFml
+            // 
+            this.txtSalaFml.Location = new System.Drawing.Point(148, 201);
+            this.txtSalaFml.Name = "txtSalaFml";
+            this.txtSalaFml.ReadOnly = true;
+            this.txtSalaFml.Size = new System.Drawing.Size(100, 23);
+            this.txtSalaFml.TabIndex = 17;
+            // 
+            // txtAluiqIPRF
+            // 
+            this.txtAluiqIPRF.Location = new System.Drawing.Point(148, 171);
+            this.txtAluiqIPRF.Name = "txtAluiqIPRF";
+            this.txtAluiqIPRF.ReadOnly = true;
+            this.txtAluiqIPRF.Size = new System.Drawing.Size(100, 23);
+            this.txtAluiqIPRF.TabIndex = 18;
+            // 
+            // txtAliqINSS
+            // 
+            this.txtAliqINSS.Location = new System.Drawing.Point(148, 132);
+            this.txtAliqINSS.Name = "txtAliqINSS";
+            this.txtAliqINSS.ReadOnly = true;
+            this.txtAliqINSS.Size = new System.Drawing.Size(100, 23);
+            this.txtAliqINSS.TabIndex = 19;
+            // 
+            // txtNumFilhos
+            // 
+            this.txtNumFilhos.Location = new System.Drawing.Point(148, 77);
+            this.txtNumFilhos.Name = "txtNumFilhos";
+            this.txtNumFilhos.Size = new System.Drawing.Size(100, 23);
+            this.txtNumFilhos.TabIndex = 20;
+            // 
+            // txtSalaBrut
+            // 
+            this.txtSalaBrut.Location = new System.Drawing.Point(148, 48);
+            this.txtSalaBrut.Name = "txtSalaBrut";
+            this.txtSalaBrut.Size = new System.Drawing.Size(100, 23);
+            this.txtSalaBrut.TabIndex = 21;
+            // 
+            // txtNomeFunci
+            // 
+            this.txtNomeFunci.Location = new System.Drawing.Point(148, 20);
+            this.txtNomeFunci.Name = "txtNomeFunci";
+            this.txtNomeFunci.Size = new System.Drawing.Size(100, 23);
+            this.txtNomeFunci.TabIndex = 22;
+            // 
+            // lblSalaLiquido
+            // 
+            this.lblSalaLiquido.AutoSize = true;
+            this.lblSalaLiquido.BackColor = System.Drawing.Color.Transparent;
+            this.lblSalaLiquido.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblSalaLiquido.Location = new System.Drawing.Point(12, 291);
+            this.lblSalaLiquido.Name = "lblSalaLiquido";
+            this.lblSalaLiquido.Size = new System.Drawing.Size(94, 15);
+            this.lblSalaLiquido.TabIndex = 23;
+            this.lblSalaLiquido.Text = "Salário Liquido  :";
+            // 
+            // txtSalaLiquido
+            // 
+            this.txtSalaLiquido.Location = new System.Drawing.Point(148, 288);
+            this.txtSalaLiquido.Name = "txtSalaLiquido";
+            this.txtSalaLiquido.ReadOnly = true;
+            this.txtSalaLiquido.Size = new System.Drawing.Size(100, 23);
+            this.txtSalaLiquido.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(728, 410);
+            this.Controls.Add(this.txtSalaLiquido);
+            this.Controls.Add(this.lblSalaLiquido);
+            this.Controls.Add(this.txtNomeFunci);
+            this.Controls.Add(this.txtSalaBrut);
+            this.Controls.Add(this.txtNumFilhos);
+            this.Controls.Add(this.txtAliqINSS);
+            this.Controls.Add(this.txtAluiqIPRF);
+            this.Controls.Add(this.txtSalaFml);
+            this.Controls.Add(this.txtDescINSS);
+            this.Controls.Add(this.txtDescIPRF);
+            this.Controls.Add(this.cmbSexo);
+            this.Controls.Add(this.cmbCasado);
+            this.Controls.Add(this.lblCasado);
+            this.Controls.Add(this.lblSexo);
+            this.Controls.Add(this.btnLimp);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnCalcu);
+            this.Controls.Add(this.lblDescIPRF);
+            this.Controls.Add(this.lblDescINSS);
+            this.Controls.Add(this.lblSalaFml);
+            this.Controls.Add(this.lblAluiqIRPF);
+            this.Controls.Add(this.lblAliqINSS);
+            this.Controls.Add(this.lblNumFilhos);
+            this.Controls.Add(this.lblSalaBrut);
+            this.Controls.Add(this.lblNomeFunci);
+            this.Name = "Form1";
+            this.Text = "Calcular Salário";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNomeFunci;
+        private System.Windows.Forms.Label lblSalaBrut;
+        private System.Windows.Forms.Label lblNumFilhos;
+        private System.Windows.Forms.Label lblAliqINSS;
+        private System.Windows.Forms.Label lblAluiqIRPF;
+        private System.Windows.Forms.Label lblSalaFml;
+        private System.Windows.Forms.Label lblDescINSS;
+        private System.Windows.Forms.Label lblDescIPRF;
+        private System.Windows.Forms.Button btnCalcu;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnLimp;
+        private System.Windows.Forms.Label lblSexo;
+        private System.Windows.Forms.Label lblCasado;
+        private System.Windows.Forms.ComboBox cmbCasado;
+        private System.Windows.Forms.ComboBox cmbSexo;
+        private System.Windows.Forms.TextBox txtDescIPRF;
+        private System.Windows.Forms.TextBox txtDescINSS;
+        private System.Windows.Forms.TextBox txtSalaFml;
+        private System.Windows.Forms.TextBox txtAluiqIPRF;
+        private System.Windows.Forms.TextBox txtAliqINSS;
+        private System.Windows.Forms.TextBox txtNumFilhos;
+        private System.Windows.Forms.TextBox txtSalaBrut;
+        private System.Windows.Forms.TextBox txtNomeFunci;
+        private System.Windows.Forms.Label lblSalaLiquido;
+        private System.Windows.Forms.TextBox txtSalaLiquido;
+    }
+}
+

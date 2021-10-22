c55e872b768e4592bc1f345dd774b6a6671fe578
Lucas Araújo <80704314+LFeli@users.noreply.github.com>
2021-10-21 21:21:28 -0300

    Add files via upload

---
+++ Atividade 7/Ptestemetodos/Ptestemetodos/frmExercicio4.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ptestemetodos
+{
+    public partial class frmExercicio4 : Form
+    {
+        public frmExercicio4()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNum_Click(object sender, EventArgs e)
+        {
+            int i;
+            double qntnums = 0;
+
+            char[] vetorTexto = rtxtInsirir.Text.ToCharArray();
+
+            for (i = 0; i < rtxtInsirir.Text.Length; i++)
+            {
+                if (char.IsNumber(vetorTexto[i]))
+                    qntnums++;
+            }
+            MessageBox.Show("O texto possui :  " + qntnums.ToString() + "  números");
+        }
+
+        private void btnLetras_Click(object sender, EventArgs e)
+        {
+            {
+                int i;
+                double qtnletras = 0;
+
+                char[] vetorTexto = rtxtInsirir.Text.ToCharArray();
+
+                for (i = 0; i < rtxtInsirir.Text.Length; i++)
+                {
+                    if (char.IsLetter(vetorTexto[i]))
+                        qtnletras++;
+                }
+                MessageBox.Show("O texto possui :  " + qtnletras.ToString() + "  letras");
+            }
+        }
+
+        private void btnBranco_Click(object sender, EventArgs e)
+        {
+            int i = 0, qtdbranco = 0;
+            char[] vetorBranco = rtxtInsirir.Text.ToCharArray();
+            bool x = true;
+
+            while (x)
+            {
+                if (Char.IsWhiteSpace(vetorBranco[i]))
+                {
+                    qtdbranco = i;
+                    x = false;
+                }
+
+                i++;
+            }
+            MessageBox.Show("O  espaço em branco  está na casa número: " + qtdbranco);
+        }
+    }
+}
---
+++ Atividade 7/Ptestemetodos/Ptestemetodos/frmExercicio5.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ptestemetodos
+{
+    public partial class frmExercicio5 : Form
+    {
+        public frmExercicio5()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGerador_Click(object sender, EventArgs e)
+        {
+            int num1;
+            int num2;
+
+            if ((int.TryParse(txtNum1.Text, out num1)) &&
+               (int.TryParse(txtNum2.Text, out num2)))
+
+            {
+                Random random = new Random();
+                double r = random.Next(num1, num2);
+                MessageBox.Show(r.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Valores invalidos");
+            }
+        }
+    }
+}

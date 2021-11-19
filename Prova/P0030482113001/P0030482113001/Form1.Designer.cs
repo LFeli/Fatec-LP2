7e4208c2131dfcfb13368c2ddb6e85b416a812d8
Lucas Araújo <80704314+LFeli@users.noreply.github.com>
2021-11-19 20:26:22 -0300

    Add files via upload

---
+++ Prova/P0030482113001/P0030482113001/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace P0030482113001
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFuncao = new System.Windows.Forms.Button();
+            this.lstbxEntrada = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnFuncao
+            // 
+            this.btnFuncao.Location = new System.Drawing.Point(312, 348);
+            this.btnFuncao.Name = "btnFuncao";
+            this.btnFuncao.Size = new System.Drawing.Size(199, 44);
+            this.btnFuncao.TabIndex = 0;
+            this.btnFuncao.Text = "Executar a função :";
+            this.btnFuncao.UseVisualStyleBackColor = true;
+            this.btnFuncao.Click += new System.EventHandler(this.btnFuncao_Click);
+            // 
+            // lstbxEntrada
+            // 
+            this.lstbxEntrada.FormattingEnabled = true;
+            this.lstbxEntrada.ItemHeight = 15;
+            this.lstbxEntrada.Location = new System.Drawing.Point(248, 140);
+            this.lstbxEntrada.Name = "lstbxEntrada";
+            this.lstbxEntrada.Size = new System.Drawing.Size(325, 94);
+            this.lstbxEntrada.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstbxEntrada);
+            this.Controls.Add(this.btnFuncao);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFuncao;
+        private System.Windows.Forms.ListBox lstbxEntrada;
+    }
+}
+
---
+++ Prova/P0030482113001/P0030482113001/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace P0030482113001
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFuncao_Click(object sender, EventArgs e)
+        {
+            double[,] vetor = new double[1, 4];
+            string semana = "";
+            double valorSemana = 0;
+            double valorMes = 0;
+            double valorGeral = 0;
+
+
+            for ( int i = 0; i < 1;i++)
+            {
+                for ( int j = 0; j < 4; j++)
+                {
+                    semana = Interaction.InputBox("Entre com o valor da semana: " + (j + 1).ToString(), "Entrada de dados");
+
+                    if (semana == "")
+                        break;
+
+                        if (double.TryParse(semana, out vetor[i, j]))
+                        {
+                            valorSemana = vetor[i, j];
+                            valorMes += vetor[i, j];
+                        }
+                            valorGeral += valorSemana;
+
+                    lstbxEntrada.Items.Insert(0, "Total do mês " + (i + 1).ToString() + " Semana " + (j + 1).ToString() + " " + valorSemana.ToString("C2"));
+                }
+                    lstbxEntrada.Items.Insert(0, "------------------------------------------------------");
+                    lstbxEntrada.Items.Insert(0,">> " + "Total do mês "  + " " + valorMes.ToString("C2"));
+                    lstbxEntrada.Items.Insert(0, "------------------------------------------------------");
+                    lstbxEntrada.Items.Insert(0,">> " + "Total geral " + " " + valorGeral.ToString("C2"));
+                    lstbxEntrada.Items.Insert(0,"------------------------------------------------------");
+            }
+        }
+    }
+}
